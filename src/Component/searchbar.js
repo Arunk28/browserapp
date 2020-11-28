0f9474f0e1134b49ac15bcd64b504de7f49329a2
arunk28 <arunkumar280491@gmail.com>
2020-11-28 20:16:04 +0530

    basic things setted up for search

--- src/Component/searchbar.js
+++ src/Component/searchbar.js
@@ -3,19 +3,27 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   state = { term: "tesd" };
+
+  onInputChange = (event)=>{
+      this.setState({term:event.target.value})
+
+  };
+  onFromSubmit =(event)=>{
+      event.preventDefault();
+      this.props.onFormSubmit(this.state.term);
+
+  }
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" onSubmit={this.onSearch}>
+        <form className="ui form" onSubmit={this.onFromSubmit}>
           <div className="field">
             <label> Video Search</label>
 
             <input
               type="text"
               value={this.state.term}
-              onChange={(e) => {
-                this.setState({ term: e.target.value });
-              }}
+              onChange={this.onInputChange}
             ></input>
           </div>
         </form>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './Component/searchbar';
-
-
-const App = () => {
-    return ( <div> <SearchBar></SearchBar></div> );
+import React, { Component } from 'react';
+class App extends Component {
+    state = {  }
+    onTermSubmit = (term)=>{
+        console.log(term);
+    }
+    render() { 
+        return ( <div> <SearchBar onFormSubmit={this.onTermSubmit}></SearchBar></div> );
+    }
 }
+ 
+export default App;
+
  
 ReactDOM.render(<App />, document.querySelector('#root'))
